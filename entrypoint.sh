735f1d6877d011f668e7e3f904683f5dbaf89b01
Lauri Kuittinen <lauri.kuittinen@reaktor.com>
2018-10-18 16:09:34 +0200

    support ambiguous characters in passwords

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-htpasswd -Bbc /etc/nginx/.htpasswd ${BASICAUTH_USERNAME} ${BASICAUTH_PASSWORD}
+htpasswd -Bbc /etc/nginx/.htpasswd "${BASICAUTH_USERNAME}" "${BASICAUTH_PASSWORD}"
 chmod 644 /etc/nginx/.htpasswd
 
 envsubst '$BACKEND_ADDRESS,$BACKEND_PORT' < /tmp/nginx.conf.tmpl \
